ae1a2d40a0d4b6423d4e63d4b314ef3a51147899
Franceline Casanas <franceline90@gmail.com>
2020-05-27 03:43:11 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,9 @@
-def parrot
-  puts "Squawk!"
-  "Squawk!"
-end
-
-def parrot(string)
-  puts string
-  string
-end
+# def parrot
+#   puts "Squawk!"
+#   "Squawk!"
+# end
+# 
+# def parrot(string)
+#   puts string
+#   string
+# end
